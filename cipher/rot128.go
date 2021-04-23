3a87c086c209f515eaac92ed2ad53a83233fea37
NatthaSiriboo <83060421+NatthaSiriboo@users.noreply.github.com>
2021-04-23 14:34:34 +0700

    Create rot128.go

---
+++ cipher/rot128.go
@@ -0,0 +1,51 @@
+package cipher
+
+import (
+	"io"
+	//"fmt"
+	//"log"
+)
+
+// Rot128Reader implements io.Reader that transforms
+//pim comment: this thing only read, not take input from command line
+//pim comment: https://golang.org/pkg/io/
+type Rot128Reader struct{ reader io.Reader }
+
+func NewRot128Reader(r io.Reader) (*Rot128Reader, error) {
+	return &Rot128Reader{reader: r}, nil
+}
+
+func (r *Rot128Reader) Read(p []byte) (int, error) {
+	if n, err := r.reader.Read(p); err != nil {
+		//fmt.Print("...=======================================.................................")
+		return n, err
+	} else {
+		//fmt.Print("..................................................................................")
+		rot128(p[:n])
+		return n, nil
+	}
+}
+
+type Rot128Writer struct {
+	writer io.Writer
+	buffer []byte // not thread-safe
+}
+
+func NewRot128Writer(w io.Writer) (*Rot128Writer, error) {
+	return &Rot128Writer{
+		writer: w,
+		buffer: make([]byte, 4096, 4096),
+	}, nil
+}
+
+func (w *Rot128Writer) Write(p []byte) (int, error) {
+	n := copy(w.buffer, p)
+	rot128(w.buffer[:n])
+	return w.writer.Write(w.buffer[:n])
+}
+
+func rot128(buf []byte) {
+	for idx := range buf {
+		buf[idx] += 128
+	}
+}
